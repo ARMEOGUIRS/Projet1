b3a6a949f0163a0ae4b097886507bda73f138ed9
ARMEOGUIRS <60424344+ARMEOGUIRS@users.noreply.github.com>
2020-01-29 12:17:35 +0100

    clonne de carré rouge

--- monJS.js
+++ monJS.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    $('h1').hide('slow');
+   // $('h1').hide('slow');
+   
+   for( var i=0; i<5; i++) {
+       $('<div class="clo-1">').appendTo( '#effets');
+   }
 });
 
